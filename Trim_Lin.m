79f7872943c3d9c088ff94a2d10c0ff4a122b0fc
dominguezAC <cf.dominguez@alumnos.upm.es>
2020-02-01 14:35:01 +0100

    some changes in trim_lin

--- Trim_Lin.m
+++ Trim_Lin.m
@@ -1,6 +1,16 @@
+% Parámetros del actuador: 
+% el actuador no debería existir en la primera parte del trabajo! (fase I),
+% aparece en la fase II y se mantiene en la fase III. 
+wnAct=20; 
+psiAct=0.8;
+maxDefAct = 0;
+minDefAct = -70;
+rateLimAct = 50;
+initialPosAct = 0; 
+initialVelAct = 0;
+
+
 Vw=10;
-wn=20;
-psi=0.8;
 Tf=0.1;
 Ke=1.3717;
 Bm=0.001;
@@ -19,10 +29,20 @@ c5=21;
 c6=0.006795;
 
 %Trim:
-Y0= 50;
-U0=0;
-iy=1;
-[X,U,Y,DX] = trim('Aerogen2019',[],U0,Y0,[],[],iy)
+X0 = [];    % initial condition on x whatever
+Y0 = 50;    % initial condition on y -> 50 (watts out)
+U0 = 0;     % initial condition on u -> 0
 
+iy = 1;     % Constrain Y(1) to be fixed
+ix = [];    % No constrain over x
+iu = [];    % No constrain over u
+
+[X,U,Y,DX] = trim('Aerogen2019',X0,U0,Y0,ix,iu,iy)
+                                
+% X(1) -> Watts
+% X(2) -> 
+% X(3) -> 
+% X(4) -> 
+%%
 %Linealization
 [num,den] = linmod('Aerogen2019',X,U)
